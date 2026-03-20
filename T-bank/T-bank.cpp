5e163b212b4b086f2d8658d46fd5ff146a8b934b
Admin <Admin@WIN-IM6IFC62LVB>
2026-03-20 11:02:08 +0700

    Добавьте файлы проекта.

---
+++ T-bank/T-bank.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    system("chcp 1251");
+    system("cls");
+
+    int start, end;
+    cin >> start >> end;
+
+    if (start >= end) {
+        return 1;
+    }
+
+    bool found = false;
+    int a = 0, b = 1;
+
+    while (a <= end) {
+        if (a >= start) {
+            cout << a << " ";
+            found = true;
+        }
+        int next = a + b;
+        a = b;
+        b = next;
+    }
+
+    if (!found) {
+        cout << "В заданном диапазоне нет чисел Фибоначчи";
+    }
+    cout << endl;
+
+    return 0;
+}
